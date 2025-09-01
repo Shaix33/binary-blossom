a0cae832c656feaf2be48ab4c030efadd245d84c
Mawela-Mpho-Precious <mphopreciousmawela@gmail.com>
2025-09-01 17:24:04 +0200

    Initial commit

--- farm/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-class LocationForm(forms.Form):
-    location = forms.CharField(
-        label="Enter a city or ZIP/postal code",
-        max_length=100,
-        widget=forms.TextInput(attrs={"placeholder": "e.g., Johannesburg or 2000"})
-    )
-
